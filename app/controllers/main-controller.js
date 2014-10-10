8e3ba4db06b3b10188d12bea6da0fabc7c76dd0b
Matthew A. Johnson <mjohnson3154@gmail.com>
2014-10-10 18:30:35 -0400

    0.0.3 - update controller

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -5,8 +5,8 @@
 
 	.controller('mainController', ['$http', function($http) {
 		this.aboutHeading = 'Welcome to My Bakery.';
-		this.aboutProjects = 'This is a place for me to show off all of my projects...';
-		this.aboutSkills = 'and for you to get to know a little about me and my skillset as a front-end developer.';
+		this.aboutProjects = 'Content Coming Soon';
+		this.aboutSkills = '';
 	}]);
 
 })();
